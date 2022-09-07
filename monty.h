be9af988fc4f3a70f098144148c112aba179ff13
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-07 20:09:32 +0100

    header debugging

--- monty.h
+++ monty.h
@@ -2,23 +2,22 @@
 #define MONTY
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct stack_s
 
-    /**
-     * struct instruction_s - opcode and its function
-     * @opcode: The opcode push pushes an element to the stack.
-     * @f: function to handle the opcode
-     *
-     * Description: opcode and its function
-     * for stack, queues, LIFO, FIFO
-     */
-    typedef struct instruction_s
+/**
+  * struct instruction_s - opcode and its function
+  * @opcode: The opcode push pushes an element to the stack.
+  * @f: function to handle the opcode
+  *
+  * Description: opcode and its function
+  * for stack, queues, LIFO, FIFO
+  */
+
+typedef struct instruction_s
 {
   char *opcode;
   void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-void _push(stack_t **stack, unsigned int line_number);
-void _pall(stack_t **stack, unsigned int line_number);
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -28,10 +27,18 @@ void _pall(stack_t **stack, unsigned int line_number);
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
+
 typedef struct stack_s
 {
   int n;
   struct stack_s *prev;
   struct stack_s *next;
 } stack_t;
+
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack);
+
+
+
+
 #endif
